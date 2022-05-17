31e2c9e618fbe626cb508c6bcbf8db4aa6d122d9
Fabio Dos Santos <fabio2st@yahoo.com>
2022-05-16 22:28:14 -0300

    solo mando el program

---
+++ VariablesOperaciones/GradosFahrenheitCs/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{ 
+    static void Main()
+    {
+        // Declaracion de variables
+        float temperaturaCelcius, temperaturaFahrenheit;
+        // Ingreso de datos
+        Console.Write("Ingrese la temperatura en grados celcius: ");
+        temperaturaCelcius = Convert.ToSingle(Console.ReadLine());
+        //temperaturaCelcius = float.Parse(Console.ReadLine());
+        // proceso
+        temperaturaFahrenheit = (float)(temperaturaCelcius * 1.8 + 32);
+        //salida
+        Console.WriteLine($"Equivale a {temperaturaFahrenheit} grados fahrenheit");
+    }
+}
