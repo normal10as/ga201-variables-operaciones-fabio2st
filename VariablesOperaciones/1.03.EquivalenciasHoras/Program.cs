9bdb922aebce8dbcae40735f30bad209f62c5b9f
Fabio Dos Santos <fabio2st@hotmail.com>
2022-09-26 13:32:26 -0300

    1.03.EquivalenciasHoras

---
+++ VariablesOperaciones/1.03.EquivalenciasHoras/Program.cs
@@ -0,0 +1,16 @@
+﻿//Declaración de variables
+ushort horas;
+uint minutos;
+uint segundos;
+ushort dias;
+//Entrada
+Console.Write("Ingrese cantidad horas: ");
+horas = Convert.ToUInt16(Console.ReadLine());
+//Proceso
+minutos = (uint)horas * 60;
+segundos = minutos * 60;
+dias = (ushort)(horas / 24);
+//Salida
+Console.WriteLine("Equivale a {0} segundos", segundos);
+Console.WriteLine("Equivale a {0} minutos", minutos);
+Console.WriteLine("Equivale a {0} dias", dias);
